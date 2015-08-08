2cbb4447ab7c112f940b200b16deb7cd5e6a5dd6
Luke Savage <lsava22@hotmail.com>
2015-08-09 07:33:47 +1000

    Item Script
    -Added ItemBase script
    -Weapon inherits from ItemBase
    -Added Passive item script (inherits from ItemBase)
    -Weapon and Passive set ItemType in their Start() functions

---
+++ DungeonCrawlersGame/Assets/Scripts/ItemBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBase : MonoBehaviour 
+{
+    public enum ItemType { WEAPON, PASSIVE };
+    public enum ItemRarity { UNCOMMON, COMMON, RARE, EPIC, LEGENDARY };
+
+    public string name;         // Item name for lookup
+    public ItemType type;
+    public ItemRarity rarity;
+    public int price;           // Vendor price
+    Sprite sprite;
+    BoxCollider2D pickupCol;    // Collider for picking up when dropped as loot
+}
--- DungeonCrawlersGame/Assets/Scripts/Weapon.cs
+++ DungeonCrawlersGame/Assets/Scripts/Weapon.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Weapon : MonoBehaviour 
+public class Weapon : ItemBase 
 {
-    public int dmg;     // Damage
-    public int cd;      // Cooldown
-    public int ammCur;  // Current Ammo
-    public int ammMax;  // Maximum Ammo
+    public int dmg;         // Damage
+    public int minDmg;      // Rarity Damage Range (Minimum)
+    public int maxDmg;      // Rarity Damage Range (Maximum)
+    public int cd;          // Cooldown
+    public int ammCur;      // Current Ammo
+    public int ammMax;      // Maximum Ammo
+
+    private PolygonCollider2D col;
+
+    void Start()
+    {
+        col = GetComponent<PolygonCollider2D>();
+        type = ItemType.WEAPON;
+    }
 
     void Attack()
     {
