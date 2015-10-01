7bdf3c3562ed8722b354d9470a3a3a5cc8e34985
CSchnitzerling <110841@student.sae.edu.au>
2015-10-01 15:27:41 +1000

    Changed Damage Struct

--- DungeonCrawlersGame/Assets/Scripts/Enemies/Enemy.cs
+++ DungeonCrawlersGame/Assets/Scripts/Enemies/Enemy.cs
@@ -2,44 +2,40 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Enemy : MonoBehaviour 
+public class Enemy : MonoBehaviour
 {
 	[SerializeField]
 	string name;
-
-	[SerializeField]
-	public List<Behaviour> enemyBehaviours = new List<Behaviour>();
 
 	public int health;
 
 	public int collisionDamage = 1;
 	public float knockbackForce = 0;
 
+	SpriteRenderer sp;
+
 
 	private Rigidbody2D rb;
 
-	public float moveSpeed;
-	public Vector2 lookDir;
-	public Vector2 moveDir;
 
+	float timerPoison = 0;
+	float timerFire = 0;
+	float timerIce = 0;
+	float timerMud = 0;
 	float damageTimer = 0;
+
+	public float effectFlashRate = 0.2f;
 
 
 	// Use this for initialization
-	void Awake () 
+	void Awake()
 	{
 		rb = gameObject.GetComponent<Rigidbody2D>();
-		moveSpeed = 0;
-		lookDir = Vector2.up;
-		moveDir = Vector2.up;
 	}
 
 	void Start()
 	{
-		for (int i = 0; i < enemyBehaviours.Count; i++)
-		{
-			enemyBehaviours[i].BehaviourStart();
-		}
+		sp = gameObject.GetComponent<SpriteRenderer>();
 
 	}
 
@@ -47,137 +43,60 @@ public class Enemy : MonoBehaviour
 	{
 		return name;
 	}
-	
-	
-	// Update is called once per frame
-	void FixedUpdate () 
+
+	float SinLerp(float tmr)
 	{
-		//Reset all variables
-		ResetSpeed();
-		//ResetLookDirection();
-		//ResetMoveDirection();
-		
-		//Run all Behaviours
-		for (int i = 0; i < enemyBehaviours.Count; i++)
-		{
-			enemyBehaviours[i].BehaviourUpdate(this);
-		}
+		return (1 + Mathf.Sin(tmr / effectFlashRate)) / 2;
+	}
 
-		//Set enemies Movement.
-		gameObject.transform.rotation = Quaternion.FromToRotation(Vector2.up, lookDir);
-		rb.velocity = moveDir.normalized * moveSpeed;
 
+	// Update is called once per frame
+	void FixedUpdate()
+	{
 		//Colour Change for taking damage.
 		if (damageTimer > 0)
 		{
-			Color spColor = Color.Lerp(Color.white, Color.red, damageTimer);
-			gameObject.GetComponent<SpriteRenderer>().color = spColor;
 			damageTimer -= Time.deltaTime;
+			Color spColor = Color.Lerp(Color.white, Color.red, damageTimer);
+			sp.color = spColor;
 		}
-	}
 
-
-	/// <summary>
-	/// All Getters
-	/// </summary>
-	/// <returns></returns>
-	public float GetSpeed()
-	{
-		return moveSpeed;
-	}
-
-	public Vector2 GetLookDirection()
-	{
-		return lookDir;
-	}
-
-	public Vector2 GetMoveDirection()
-	{
-		return moveDir;
-	}
-	public float GetRbVelocity()
-	{
-		return rb.velocity.magnitude;
-	}
-
-
-
-	/// <summary>
-	/// All Setters
-	/// </summary>
-	/// <param name="sp"></param>
-	public void SetSpeed(float sp)
-	{
-		if (sp > moveSpeed)
+		if (timerPoison > 0)
 		{
-			moveSpeed = sp;
+			timerPoison -= Time.deltaTime;
+			Color spColor = Color.Lerp(Color.white, Color.green, SinLerp(timerPoison));
+			sp.color = spColor;
+		}
+
+		if (timerFire > 0)
+		{
+
+
+		}
+
+		if (timerIce > 0)
+		{
+
+
+		}
+
+		if (timerMud > 0)
+		{
+
+
 		}
 	}
-
-	public void SetLookDirection(float angle)
-	{
-		lookDir = GLobalFunctions.DegToVector(angle);
-	}
-	public void SetLookDirection(Vector2 dir)
-	{
-		lookDir = dir;
-	}
-
-	public void RotateLookDirection(float angle)
-	{
-		GLobalFunctions.RotateVector(ref lookDir, angle);
-	}
-
-	public void SetMoveDirection(float angle)
-	{
-		moveDir = GLobalFunctions.DegToVector(angle).normalized;
-	}
-
-	public void SetMoveDirection(Vector2 m_dir)
-	{
-		moveDir = m_dir.normalized;
-	}
-
-	public void RotateMoveDirection(float angle)
-	{
-		GLobalFunctions.RotateVector(ref moveDir, angle);
-	}
-
-
-	/// <summary>
-	/// Resets the speed to the speed at the start of the update.
-	/// </summary>
-	public void ResetSpeed()
-	{
-		moveSpeed = rb.velocity.magnitude;
-	}
-	public void ResetLookDirection()
-	{
-		lookDir = transform.rotation * Vector2.up;
-	}
-	public void ResetMoveDirection()
-	{
-		moveDir = rb.velocity.normalized;
-	}
-
 
 	/// <summary>
 	/// Called When the Enemy Takes any damage/hit
 	/// Damge could be = to 0
 	/// </summary>
 	/// <param name="dam"></param>
-	public void OnTakeDamage(int dam, Vector2 knockbackForce)
+	public void OnTakeDamage(Damage dam)
 	{
-		health -= dam;
+		health -= dam.amount;
 
 		damageTimer = 1;
-
-		//Loop through Behaviours and call on take damage
-		for (int i = 0; i < enemyBehaviours.Count; i++)
-		{
-			enemyBehaviours[i].OnTakeDamage(dam, knockbackForce);
-		}
-
 
 		if (health <= 0)
 		{
@@ -190,13 +109,8 @@ public class Enemy : MonoBehaviour
 		//if(GameManager.inst.gameDataManager != null)
 		GameManager.inst.gameDataManager.allEnemiesKilled.Add(this);
 
-		if(GameManager.inst.questManager.currentQuest != null)
+		if (GameManager.inst.questManager.currentQuest != null)
 			DisplayQuestText();
-		//Loop through Behaviours and call on death
-		for (int i = 0; i < enemyBehaviours.Count; i++)
-		{
-			enemyBehaviours[i].OnDeath();
-		}
 
 		gameObject.SetActive(false);
 
@@ -214,43 +128,43 @@ public class Enemy : MonoBehaviour
 		tm.text = GameManager.inst.questManager.currentQuest.GetQuestCounterText();
 	}
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "Player")
-        {
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
 			Vector2 kbForce = (col.gameObject.transform.position - gameObject.transform.position).normalized;
-            col.gameObject.GetComponent<Player>().OnTakeDamage(collisionDamage, kbForce * knockbackForce );
-        }
-        if (col.gameObject.tag == "Enemy")
-        {
-            //Enmemies do not take collision damage from other enemies.
-        }
+			col.gameObject.GetComponent<Player>().OnTakeDamage(collisionDamage, kbForce * knockbackForce);
+		}
+		if (col.gameObject.tag == "Enemy")
+		{
+			//Enmemies do not take collision damage from other enemies.
+		}
 
-        // If we standardize the damage call
-        //col.gameObject.GetComponent<Damageable>().TakeDamage(damage, transform.position, knockback);
-    }
+		// If we standardize the damage call
+		//col.gameObject.GetComponent<Damageable>().TakeDamage(damage, transform.position, knockback);
+	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-        //if (col.gameObject.tag == "WepLeft")
-        //{
-        //    Weapon wep = GameManager.inst.activeItems.wepLeft.GetComponent<Weapon>();
-        //    Vector2 kbForce = -(col.gameObject.transform.position - gameObject.transform.position).normalized;
-        //    OnTakeDamage(wep.dmg, kbForce);// * wep.kb);
-        //}
-		if (col.gameObject.tag == "WepRight")
-		{
-			Weapon wep = GameManager.inst.activeItems.wepRight.GetComponent<Weapon>();
-			Vector2 kbForce = -(col.gameObject.transform.position - gameObject.transform.position).normalized;
-			OnTakeDamage(wep.dmg, kbForce);// * wep.kb);
-		}
+		//if (col.gameObject.tag == "WepLeft")
+		//{
+		//    Weapon wep = GameManager.inst.activeItems.wepLeft.GetComponent<Weapon>();
+		//    Vector2 kbForce = -(col.gameObject.transform.position - gameObject.transform.position).normalized;
+		//    OnTakeDamage(wep.dmg, kbForce);// * wep.kb);
+		//}
+		//if (col.gameObject.tag == "WepRight")
+		//{
+		//	Weapon wep = GameManager.inst.activeItems.wepRight.GetComponent<Weapon>();
+		//	Vector2 kbForce = -(col.gameObject.transform.position - gameObject.transform.position).normalized;
+		//	OnTakeDamage(wep.dmg, kbForce);// * wep.kb);
+		//}
 
-        if(col.gameObject.tag == "Projectile")
-        {
-            Vector2 kbForce = -(col.gameObject.transform.position - gameObject.transform.position).normalized;
-            OnTakeDamage(col.gameObject.GetComponent<Projectile>().dmg, kbForce);
-            Destroy(col.gameObject);
-        }
+		//if(col.gameObject.tag == "Projectile")
+		//{
+		//	Vector2 kbForce = -(col.gameObject.transform.position - gameObject.transform.position).normalized;
+		//	OnTakeDamage(col.gameObject.GetComponent<Projectile>().dmg, kbForce);
+		//	Destroy(col.gameObject);
+		//}
 	}
 
 	//void OnTriggerEnter2D(Collider2D col)
