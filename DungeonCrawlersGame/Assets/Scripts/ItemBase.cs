9bf09b623b370d0ea85eb61c089069178e5f54c2
LukeSavage <lsava22@hotmail.com>
2015-10-02 11:35:47 +1000

    Started writing weapon drops

--- DungeonCrawlersGame/Assets/Scripts/ItemBase.cs
+++ DungeonCrawlersGame/Assets/Scripts/ItemBase.cs
@@ -13,4 +13,15 @@ public class ItemBase : MonoBehaviour
     public int price;           // Vendor price
     Sprite sprite;
     BoxCollider2D pickupCol;    // Collider for picking up when dropped as loot
+
+    // THIS GOES ON THE PLAYER WHEN COREY STOPS TOUCHING IT
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            // Instantiate current equipped and drop
+            Instantiate(GameManager.inst.activeItems.wepRight, transform.position, transform.rotation);
+
+        }
+    }
 }
